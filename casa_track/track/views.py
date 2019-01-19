b00579acb1e6d6009ffe18a8c4e622175be30a46
elenarfowler <elenarfowler@gmail.com>
2019-01-19 15:01:48 -0800

    Adding Tracking Forms Page

--- casa_track/track/views.py
+++ casa_track/track/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
    return render(request, 'track/index.html')
+
+def tracking(request):
+    return render(request, 'track/tracking.html')

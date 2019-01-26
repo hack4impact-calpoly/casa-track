3fe2375228c8715f7b19dded711ac1a1fa821e4a
bglossner <benglossner@gmail.com>
2019-01-26 14:14:59 -0800

    test page and css

--- casa_track/casa_track/urls.py
+++ casa_track/casa_track/urls.py
@@ -20,6 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', include('track.urls')),
     path('', include('track.urls')),
 ]
 
--- casa_track/track/views.py
+++ casa_track/track/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+   print("PATH BELOW:")
+   if request.path == "/test":
+      return render(request, 'track/test.html')
    return render(request, 'track/index.html')

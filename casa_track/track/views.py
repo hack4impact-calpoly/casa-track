78fadd8f1433a133a300a0e54eb541b99470063c
Eric <enewcomer23@gmail.com>
2019-02-23 12:47:54 -0800

    Format admin html page

--- casa_track/track/views.py
+++ casa_track/track/views.py
@@ -21,7 +21,6 @@ def tracking(request):
       form = TrackingFormForm(request.POST)
       if form.is_valid():
          form.save()
-         
          return redirect('/')
       else:
          print(form.errors)
@@ -41,14 +40,5 @@ def delete_form(request):
     else:
         return HttpResponseNotAllowed(request)
 
-def pdf_view(request):
-    fs = FileSystemStorage()
-    filename = 'mypdf.pdf'
-    if fs.exists(filename):
-        with fs.open(filename) as pdf:
-            response = HttpResponse(pdf, content_type='application/pdf')
-            response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
-            return response
-    else:
-        return HttpResponseNotFound('The requested pdf was not found in our server.')
+
 

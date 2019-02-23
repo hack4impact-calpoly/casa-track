8359a7a73f98836347b15ee9c534550ee552b0ea
Eric <enewcomer23@gmail.com>
2019-02-23 12:30:07 -0800

    Add admin stuff

--- casa_track/track/urls.py
+++ casa_track/track/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('tracking/', views.tracking, name='tracking'),
+    path('delete_form', views.delete_form, name='delete_form'),
 ]
